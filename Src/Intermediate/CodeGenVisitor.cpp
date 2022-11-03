fb09535d58790c4c837bcb77db2fa12c33de3956
Slowlor1ss <laurens.krikilion@hotmail.com>
2022-11-03 15:01:07 +0100

    InitialPush

---
+++ Src/Intermediate/CodeGenVisitor.cpp
@@ -0,0 +1,1 @@
+#include "CodeGenVisitor.h"
---
+++ Src/Intermediate/CodeGenVisitor.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "antlr4-runtime.h"
+#include "TParserBaseVisitor.h"
+
+class CodeGenVisitor : public antlrcpptest::TParserBaseVisitor
+{
+};
+
